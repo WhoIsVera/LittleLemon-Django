e00b6a2fe6c5eb88c277b0e63c130a96999240a6
WhoIsVera <ev765030@gmail.com>
2024-11-02 15:03:18 -0600

    Created Menu model, serializer, views, and configured API endpoints

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} : {str(self.price)}"
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
